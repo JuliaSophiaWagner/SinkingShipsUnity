412a435bb91036d7466fd6d15f40d1edd55e17e1
Christoph Enke <110046@fhwn.ac.at>
2021-02-17 18:41:56 +0100

    addes model classes

---
+++ SinkingShipsServer/SinkingShipsServer.Database/Models/Clientdata.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SinkingShipsServer.Database.Models
+{
+    public class Clientdata
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Token
+        {
+            get;
+            set;
+        }
+
+        public string ID
+        {
+            get;
+            private set;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        public string Password
+        {
+            get;
+            private set;
+        }
+
+        public List<Player> GameRequests
+        {
+            get;
+            set;
+        }
+
+        public List<History> History
+        {
+            get;
+            set;
+        }
+
+        public int Won
+        {
+            get;
+            set;
+        }
+
+        public int Points
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ SinkingShipsServer/SinkingShipsServer.Database/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SinkingShipsServer.Database.Models
+{
+    public class Player
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name { get; set; }
+
+        public string ID { get; set; }
+
+        public string Token { get; set; }
+    }
+}
